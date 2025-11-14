1b27fa171fcb324084769d4a96e2dbd758244c38
Alex <a4l3e2x1@hotmail.com>
2025-11-14 21:42:10 +0100

    solucionado error navbar con hashlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,97 +1,107 @@
 import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 export function Navbar({ favorites = [] }) {
   return (
-    <nav id="mainNav" className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <nav
+      id="mainNav"
+      className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top"
+    >
       <div className="container-fluid">
-        
-        
-        <Link className="navbar-brand" to="/" >
-          <img 
-            src="public\assets\starwarsLogo.png" 
-            alt="Star Wars Logo" 
-            width="80" 
-            style={{ height: 'auto' }}
+        <Link className="navbar-brand" to="/">
+          <img
+            src="../../public/assets/starwarsLogo.png"
+            alt="Star Wars Logo"
+            width="80"
+            style={{ height: "auto" }}
           />
         </Link>
-        
-        
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarLinks" 
-          aria-controls="navbarLinks" 
-          aria-expanded="false" 
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarLinks"
+          aria-controls="navbarLinks"
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        
-        
+
         <div className="collapse navbar-collapse" id="navbarLinks">
-          
-         
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
             <li className="nav-item">
-				<a className="nav-link" href="#gente">People</a>
+              <HashLink to="/#gente" className="nav-link">
+                People
+              </HashLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#peliculas">Films</a>
+              <HashLink className="nav-link"  to="/#peliculas" smooth>
+                Films
+              </HashLink>
+            </li>
+
+            <li className="nav-item">
+              <HashLink to="/#planetas" className="nav-link" >
+                Planets
+              </HashLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#planetas">Planets</a>
+              <HashLink to="/#especies" className="nav-link" >
+                Species
+              </HashLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#especies">Species</a>
+              <HashLink to="/#naves" className="nav-link">
+                Starships
+              </HashLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#naves">Starships</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#vehiculos">Vehicles</a>
+              <HashLink to="/#vehiculos" className="nav-link">
+                Vehicles
+              </HashLink>
             </li>
           </ul>
 
-          
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item dropdown">
-              <a 
-                className="nav-link dropdown-toggle" 
-                href="#" 
-                role="button" 
-                data-bs-toggle="dropdown" 
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Favorites 
-                
+                Favorites
                 <span className="badge bg-danger ms-1">{favorites.length}</span>
               </a>
-              
-             
-              <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-               
-                {
-                  favorites.length > 0 ? (
-                    favorites.map(fav => (
-                      <li key={fav.id}>
-                        <a className="dropdown-item" href={`#/${fav.type}/${fav.id}`}>
-                          {fav.name}
-                        </a>
-                      </li>
-                    ))
-                  ) : (
-                    
-                    <li><span className="dropdown-item-text">No hay favoritos</span></li>
-                  )
-                }
+
+              <ul
+                className="dropdown-menu dropdown-menu-end"
+                aria-labelledby="navbarDropdown"
+              >
+                {favorites.length > 0 ? (
+                  favorites.map((fav) => (
+                    <li key={fav.id}>
+                      <a
+                        className="dropdown-item"
+                        href={`#/${fav.type}/${fav.id}`}
+                      >
+                        {fav.name}
+                      </a>
+                    </li>
+                  ))
+                ) : (
+                  <li>
+                    <span className="dropdown-item-text">No hay favoritos</span>
+                  </li>
+                )}
               </ul>
             </li>
-          </ul> 
-          
-        </div> 
-      </div> 
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
-
